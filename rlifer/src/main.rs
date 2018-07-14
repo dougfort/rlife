8b7b487b189d8f7f08eb141a81f9262646733852
dougfort <doug.fort@gmail.com>
2018-07-13 20:55:08 -0400

    create the initial generation vector in a cleaner way.

--- rlifer/src/main.rs
+++ rlifer/src/main.rs
@@ -6,15 +6,12 @@ use rlife_game::{Cell, step};
 
 fn main() {
  
-    let c1 = Cell::from_str("(-1, 0)").unwrap();
-    let c2 = Cell::from_str("(0, 0)").unwrap();
-    let c3 = Cell::from_str("(1, 0)").unwrap();
 
-    let mut generation = vec![];
-
-    generation.push(c1);
-    generation.push(c2);
-    generation.push(c3);
+    let generation = vec![
+        Cell::from_str("(-1, 0)").unwrap(),
+        Cell::from_str("(0, 0)").unwrap(),
+        Cell::from_str("(1, 0)").unwrap()
+    ];
     println!("g[{}] = {:?}", 0, generation);
 
     for i in 0..10 {
