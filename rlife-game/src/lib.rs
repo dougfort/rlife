81f876b27f348590f86c6ed3d2415134389e0ab9
dougfort <doug.fort@deciphernow.com>
2019-09-06 14:57:50 -0400

    update to edition = "2018" (#1)
    clean up with cargo fmt and cargo clippy

--- rlife-game/src/lib.rs
+++ rlife-game/src/lib.rs
@@ -1,42 +1,49 @@
 use std::collections::HashMap;
-use std::str::FromStr;
 use std::num::ParseIntError;
+use std::str::FromStr;
 
 #[derive(Copy, Clone, Debug, PartialEq, Eq, Hash)]
 pub struct Cell {
     pub x: i32,
-    pub y: i32
+    pub y: i32,
 }
 
 impl FromStr for Cell {
     type Err = ParseIntError;
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
-        let coords: Vec<&str> = s.trim_matches(|p| p == '(' || p == ')' )
-                                 .split(",")
-                                 .collect();
+        let coords: Vec<&str> = s
+            .trim_matches(|p| p == '(' || p == ')')
+            .split(',')
+            .collect();
 
         let x_fromstr = coords[0].trim().parse::<i32>()?;
         let y_fromstr = coords[1].trim().parse::<i32>()?;
 
-        Ok(Cell { x: x_fromstr, y: y_fromstr })
+        Ok(Cell {
+            x: x_fromstr,
+            y: y_fromstr,
+        })
     }
 }
 
 #[derive(Debug)]
 struct CellState {
-    live : bool,
-    count: u32
+    live: bool,
+    count: u32,
 }
 
-pub fn step(current: &Vec<Cell>) -> Vec<Cell> {
+pub fn step(current: &[Cell]) -> Vec<Cell> {
     let mut neighbor_map: HashMap<Cell, CellState> = HashMap::new();
 
     for gen_cell in current {
-        for x in [(gen_cell.x-1), gen_cell.x, (gen_cell.x+1)].iter() {
-            for y in [(gen_cell.y-1), gen_cell.y, (gen_cell.y+1)].iter() {
-                let neighbor_cell = Cell{x: *x, y: *y};
-                let ref mut state = neighbor_map.entry(neighbor_cell).or_insert(CellState{live: false, count:0});
+        for x in [(gen_cell.x - 1), gen_cell.x, (gen_cell.x + 1)].iter() {
+            for y in [(gen_cell.y - 1), gen_cell.y, (gen_cell.y + 1)].iter() {
+                let neighbor_cell = Cell { x: *x, y: *y };
+                let state = &mut neighbor_map.entry(neighbor_cell).or_insert(CellState {
+                    live: false,
+                    count: 0,
+                });
                 if neighbor_cell == *gen_cell {
                     state.live = true;
                 } else {
@@ -48,12 +55,15 @@ pub fn step(current: &Vec<Cell>) -> Vec<Cell> {
 
     let mut next = vec![];
 
-    for (cell, state) in neighbor_map {    
+    for (cell, state) in neighbor_map {
         let lives = match state {
-            CellState{live: true, count: 2} => true,
-            CellState{live: _, count: 3} => true,
-            _ => false
-        }; 
+            CellState {
+                live: true,
+                count: 2,
+            } => true,
+            CellState { count: 3, .. } => true,
+            _ => false,
+        };
         if lives {
             next.push(cell);
         }
@@ -61,4 +71,3 @@ pub fn step(current: &Vec<Cell>) -> Vec<Cell> {
 
     next
 }
-
--- rlifer/src/main.rs
+++ rlifer/src/main.rs
@@ -1,22 +1,17 @@
-extern crate rlife_game;
-
 use std::str::FromStr;
 
-use rlife_game::{Cell, step};
+use rlife_game::{step, Cell};
 
 fn main() {
- 
-
     let generation = vec![
         Cell::from_str("(-1, 0)").unwrap(),
         Cell::from_str("(0, 0)").unwrap(),
-        Cell::from_str("(1, 0)").unwrap()
+        Cell::from_str("(1, 0)").unwrap(),
     ];
-    println!("g[{}] = {:?}", 0, generation);
+    println!("g[0] = {:?}", generation);
 
     for i in 0..10 {
         let generation = step(&generation);
-        println!("g[{}] = {:?}", i+1, generation);
+        println!("g[{}] = {:?}", i + 1, generation);
     }
-
 }
